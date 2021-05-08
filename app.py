99f35850d39e1df351eded6c9910c4852dac5975
KWON-IRIS <joohee.kwon0@gmail.com>
2021-05-08 14:42:49 +0900

    크롤링 정보저장

--- app.py
+++ app.py
@@ -1,14 +1,60 @@
-from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, jsonify
 
 # 플라스크 웹 서버 생성하기
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+# mongodb 추가
+client = MongoClient('localhost', 27017)
+db = client.get_database('sparta')
 
 
 # api 추가
 # {{렌더링 영역}} : flask 문법
 @app.route('/', methods=['GET'])  # 데코레이터 문법
-def index(): # 함수이름은 고유해야함
+def index():  # 함수이름은 고유해야함
     return render_template('index.html', test='테스트')
+
+
+# 아티클 추가 api
+@app.route('/', methods=['POST'])
+def save_memo():
+    # alt+enter > flask.request 추가
+    form = request.form
+    url_receive = form['url_give']
+    comment_receive = form['comment_give']
+
+   # print(url_receive, comment_receive)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+    response = requests.get(
+        url_receive,
+        headers=headers
+    )
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')
+    url = soup.select_one('meta[property="og:url"]')
+    description = soup.select_one('meta[property="og:description"]')
+    img = soup.select_one('meta[property="og:image"]')
+
+    print(title['content'])
+
+    document = {
+        'title': title['content'],
+        'image': img['content'],
+        'url': url['content'],
+        'description': description['content'],
+        'comment': comment_receive,
+    }
+    db.articles.insert_one(document)
+    return jsonify(
+        {'result': 'success'}
+    )
 
 
 # app.py 파일을 직접 실행시킬떄 동작시킴
