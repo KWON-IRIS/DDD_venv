fccb1bed08c36a28f101d76510dd296a5f34e920
KWON-IRIS <joohee.kwon0@gmail.com>
2021-05-08 13:38:00 +0900

    index.html 렌더링 > flask 문법을 이용한 이름 수정

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # 플라스크 웹 서버 생성하기
 app = Flask(__name__)
 
+
+# api 추가
+# {{렌더링 영역}} : flask 문법
+@app.route('/', methods=['GET'])  # 데코레이터 문법
+def index(): # 함수이름은 고유해야함
+    return render_template('index.html', test='테스트')
+
+
 # app.py 파일을 직접 실행시킬떄 동작시킴
+# 항상 가장 아래 있어야함
+# localhost: 7000 >> 브라우저 내에서 수정해야함.
 if __name__ == '__main__':
     app.run(
         '0.0.0.0',  # 모든 IP에서 오는 요청을 허용
